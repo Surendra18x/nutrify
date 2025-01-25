b159234cc82d4562dadaaae5d1fecdf6680ebce8
Surendra18x <vermasurendra074@gmail.com>
2025-01-25 13:08:36 +0530

    latest commit

--- src/pages/Home/Components/categoryFilter.jsx
+++ src/pages/Home/Components/categoryFilter.jsx
@@ -9,15 +9,15 @@ export const CategoryFilter = ({Categories,selectedCategory,setSelectedCategory}
     }
 
     return(
-        <div className="category-filter">
+        <div className="flex justify-center items-center flex-col my-5">
             <select
             value={selectedCategory}
             onChange={(e) => handleCategoryChange(e.target.value)}
-            className="category-select"
+            className="w-full max-w-[400px] p-2.5 text-[16px] border border-gray-300 rounded-lg bg-gray-100 text-gray-800 outline-none transition focus:border-blue-500 sm:w-[90%] sm:text-[14px] xs:w-full xs:text-[12px]"
             >
                 <option value="">All Categories</option>
                 {Categories.map((category) => (
-                    <option value={category}>
+                    <option value={category} className="text-[16px] text-gray-800">
                         {category}
                     </option>
                 ))}
